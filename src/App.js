eb4a5620ae194560eb959268c54f0e65b2952895
Arthur BABEY <arthur.babey@icloud.com>
2017-08-11 12:20:52 +0200

    Lecture 59
    Signed-off-by: Arthur BABEY <arthur.babey@icloud.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
+
 import { Header } from './components/common';
 
 class App extends Component {
@@ -10,6 +12,19 @@ class App extends Component {
         <Text>Coucou</Text>
       </View>
     );
+  }
+
+  // Lifecycle methods
+  componentWillMount() {
+    // Initialize firebase
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCEURs_OS9nQtK-boCP7VIUDzKFiiJTS90',
+      authDomain: 'auth-afa4f.firebaseapp.com',
+      databaseURL: 'https://auth-afa4f.firebaseio.com',
+      projectId: 'auth-afa4f',
+      storageBucket: 'auth-afa4f.appspot.com',
+      messagingSenderId: '414189996447',
+    });
   }
 }
 
